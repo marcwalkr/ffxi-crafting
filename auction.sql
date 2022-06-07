d94dba23c99a174b19c942b05c51f960d46b9729
Marcus Walker <marcwalkr@gmail.com>
2022-06-07 16:46:17 -0500

    Remove duplicate auction.sql

--- auction.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS `auction`;
-CREATE TABLE `auction` (
-	`itemid` smallint(5) UNSIGNED PRIMARY KEY,
-    `single_price` int(10) UNSIGNED,
-    `stack_price` int(10) UNSIGNED,
-    `single_frequency` float(5,3) UNSIGNED,
-    `stack_frequency` float(5,3) UNSIGNED,
-    `last_updated` timestamp DEFAULT current_timestamp ON UPDATE current_timestamp
-);
