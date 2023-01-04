9999bfe524e3a0ca7c3fc132260e1f84622065b8
Marcus Walker <marcwalkr@gmail.com>
2023-01-03 23:20:27 -0600

    Filter recipes by craftable

--- ffxicrafting/crafting_table.py
+++ ffxicrafting/crafting_table.py
@@ -1,11 +1,16 @@
 from controllers.synth_controller import SynthController
+from synth import Synth
 
 
 class CraftingTable:
     def __init__(self, crafter, frequency_threshold,
                  sort_column, reverse_sort) -> None:
-        self.recipes = SynthController.get_all_recipes()
         self.crafter = crafter
+
+        all_recipes = SynthController.get_all_recipes()
+        synths = [Synth(r, crafter) for r in all_recipes]
+        self.recipes = [s.recipe for s in synths if s.can_craft]
+
         self.frequency_threshold = frequency_threshold
         self.sort_column = sort_column
         self.reverse_sort = reverse_sort
