fa97c4f6e6a57daa826a4be05f9f9b89cb8ff254
marcwalkr <47399441+marcwalkr@users.noreply.github.com>
2024-07-10 16:05:21 -0500

    Add data processing to ScreenshotAnalyzer

--- ffxicrafting/screenshot_analyzer.py
+++ ffxicrafting/screenshot_analyzer.py
@@ -1,5 +1,8 @@
 import pytesseract
 import re
+import pandas as pd
+import statistics
+import os
 from PIL import Image
 
 
@@ -7,13 +10,22 @@ class ScreenshotAnalyzer:
     def __init__(self, item_name) -> None:
         self.item_name = item_name
 
-    def get_single_data(self):
-        single_path = "auction_screenshots/{}.png".format(self.item_name)
-        return self.extract_data(single_path)
+    def get_price_and_frequency(self, stack):
+        if stack:
+            path = "auction_screenshots/{}_stack.png".format(self.item_name)
+        else:
+            path = "auction_screenshots/{}.png".format(self.item_name)
 
-    def get_stack_data(self):
-        stack_path = "auction_screenshots/{}_stack.png".format(self.item_name)
-        return self.extract_data(stack_path)
+        # If file doesn't exist, return None for price and frequency
+        if not os.path.isfile(path):
+            return None, None
+
+        timestamp_strings, prices = self.extract_data(path)
+
+        avg_price = statistics.fmean(prices)
+        sales_frequency = self.calculate_sales_frequency(timestamp_strings)
+
+        return avg_price, sales_frequency
 
     @staticmethod
     def extract_data(path):
@@ -49,3 +61,21 @@ class ScreenshotAnalyzer:
                 prices.append(price_int)
 
         return timestamps, prices
+
+    @staticmethod
+    def calculate_sales_frequency(timestamp_strings):
+        # Convert to datetime
+        timestamps = pd.to_datetime(timestamp_strings, format="%m/%d/%Y, %I:%M:%S %p")
+
+        # Sort timestamps
+        timestamps = timestamps.sort_values()
+
+        # Calculate time differences
+        time_diffs = timestamps.diff().dropna()
+
+        # Average time difference
+        avg_time_diff = time_diffs.mean()
+
+        # Convert average time differences to frequency (sales per day)
+        avg_time_diff_seconds = avg_time_diff.total_seconds()
+        return 86400 / avg_time_diff_seconds if avg_time_diff_seconds != 0 else None
