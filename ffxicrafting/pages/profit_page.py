16c211527479c33a7a6c15e2ee189eccc7248439
marcwalkr <47399441+marcwalkr@users.noreply.github.com>
2024-07-16 01:23:23 -0500

    Add threading and progress bars

--- ffxicrafting/pages/profit_page.py
+++ ffxicrafting/pages/profit_page.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+import threading
 from utils.widgets import TreeviewWithSort
 from controllers.recipe_controller import RecipeController
 from entities.crafter import Crafter
@@ -15,16 +16,67 @@ class ProfitPage(RecipeListPage):
     def create_profit_page(self):
         self.parent.notebook.add(self, text="Profit Table")
 
-        generate_button = ttk.Button(self, text="Generate Table", command=self.generate_profit_table)
-        generate_button.pack(pady=10)
+        self.generate_button = ttk.Button(self, text="Generate Table", command=self.start_generate_profit_table)
+        self.generate_button.pack(pady=10)
 
-        self.profit_tree = TreeviewWithSort(self, columns=(
-            "nq", "hq", "tier", "cost_per_synth", "profit_per_synth", "profit_per_storage"), show="headings")
+        self.progress = ttk.Progressbar(self, mode='indeterminate', length=300)
+        self.progress.pack(pady=10)
+        self.progress.pack_forget()
+
+        self.profit_tree = TreeviewWithSort(self, columns=("nq", "hq", "tier", "cost_per_synth", "profit_per_synth",
+                                                           "profit_per_storage"), show="headings")
         self.configure_profit_treeview(self.profit_tree)
         self.profit_tree.pack(padx=10, pady=10, expand=True, fill="both")
 
         self.profit_tree.bind("<Double-1>", self.show_recipe_details)
         self.profit_tree.bind("<Button-1>", self.on_treeview_click)
+
+    def start_generate_profit_table(self):
+        self.generate_button.config(state=tk.DISABLED)
+        self.clear_treeview(self.profit_tree)
+        self.progress.pack_forget()
+        self.progress.pack(pady=10, before=self.profit_tree)
+        self.progress.start()
+        threading.Thread(target=self.query_recipes).start()
+
+    def query_recipes(self):
+        skills = SettingsManager.get_skills()
+        skill_look_ahead = SettingsManager.get_skill_look_ahead()
+        recipes = RecipeController.get_recipes_by_craft_levels(*(skill - skill_look_ahead for skill in skills))
+        self.synth_profit_threshold = SettingsManager.get_profit_per_synth()
+        self.storage_profit_threshold = SettingsManager.get_profit_per_storage()
+        self.simulation_trials = SettingsManager.get_simulation_trials()
+        self.total_recipes = len(recipes)
+        self.recipes = recipes
+        self.after(10, self.update_profit_progress, 0, [])
+
+    def update_profit_progress(self, index, batch):
+        if index < self.total_recipes:
+            recipe = self.recipes[index]
+            crafter = Crafter(*SettingsManager.get_skills(), recipe)
+
+            crafter.synth.cost = crafter.synth.calculate_cost()
+            if crafter.synth.cost is not None:
+                profit_per_synth, profit_per_storage = crafter.craft(self.simulation_trials)
+                if profit_per_synth >= self.synth_profit_threshold and profit_per_storage >= self.storage_profit_threshold:
+                    nq_string = recipe.get_formatted_nq_result()
+                    hq_string = recipe.get_formatted_hq_results()
+                    row = [nq_string, hq_string, crafter.synth.tier,
+                           crafter.synth.cost, profit_per_synth, profit_per_storage]
+                    batch.append((recipe.id, row))
+
+            if len(batch) >= 10:
+                for item in batch:
+                    self.profit_tree.insert("", "end", iid=item[0], values=item[1])
+                batch.clear()
+
+            self.after(10, self.update_profit_progress, index + 1, batch)
+        else:
+            for item in batch:
+                self.profit_tree.insert("", "end", iid=item[0], values=item[1])
+            self.progress.stop()
+            self.progress.pack_forget()
+            self.generate_button.config(state=tk.NORMAL)
 
     def configure_profit_treeview(self, treeview):
         treeview.heading("nq", text="NQ")
@@ -34,35 +86,7 @@ class ProfitPage(RecipeListPage):
         treeview.heading("profit_per_synth", text="Profit / Synth")
         treeview.heading("profit_per_storage", text="Profit / Storage")
 
-        # Center columns
         treeview.column("tier", anchor=tk.CENTER)
         treeview.column("cost_per_synth", anchor=tk.CENTER)
         treeview.column("profit_per_synth", anchor=tk.CENTER)
         treeview.column("profit_per_storage", anchor=tk.CENTER)
-
-    def generate_profit_table(self):
-        self.clear_treeview(self.profit_tree)
-
-        skills = SettingsManager.get_skills()
-        skill_look_ahead = SettingsManager.get_skill_look_ahead()
-        recipes = RecipeController.get_recipes_by_craft_levels(*(skill - skill_look_ahead for skill in skills))
-        synth_profit_threshold = SettingsManager.get_profit_per_synth()
-        storage_profit_threshold = SettingsManager.get_profit_per_storage()
-        simulation_trials = SettingsManager.get_simulation_trials()
-
-        for recipe in recipes:
-            crafter = Crafter(*skills, recipe)
-
-            crafter.synth.cost = crafter.synth.calculate_cost()
-            if crafter.synth.cost is None:
-                continue
-
-            profit_per_synth, profit_per_storage = crafter.craft(simulation_trials)
-            if profit_per_synth < synth_profit_threshold or profit_per_storage < storage_profit_threshold:
-                continue
-
-            nq_string = recipe.get_formatted_nq_result()
-            hq_string = recipe.get_formatted_hq_results()
-            row = [nq_string, hq_string, crafter.synth.tier,
-                   crafter.synth.cost, profit_per_synth, profit_per_storage]
-            self.profit_tree.insert("", "end", iid=recipe.id, values=row)
--- ffxicrafting/pages/search_page.py
+++ ffxicrafting/pages/search_page.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+import threading
 from utils.widgets import TreeviewWithSort
 from controllers.recipe_controller import RecipeController
 from pages.recipe_list_page import RecipeListPage
@@ -13,12 +14,16 @@ class SearchPage(RecipeListPage):
     def create_search_page(self):
         self.parent.notebook.add(self, text="Search Recipes")
 
-        search_var = tk.StringVar()
-        search_entry = ttk.Entry(self, textvariable=search_var, font=("Helvetica", 14))
-        search_entry.pack(pady=10)
-        search_button = ttk.Button(self, text="Search",
-                                   command=lambda: self.search_recipes(search_var.get()))
-        search_button.pack(pady=(0, 10))
+        self.search_var = tk.StringVar()
+        self.search_entry = ttk.Entry(self, textvariable=self.search_var, font=("Helvetica", 14))
+        self.search_entry.pack(pady=10)
+        self.search_button = ttk.Button(self, text="Search",
+                                        command=self.start_search_recipes)
+        self.search_button.pack(pady=(0, 10))
+
+        self.search_progress = ttk.Progressbar(self, mode='indeterminate', length=300)
+        self.search_progress.pack(pady=10)
+        self.search_progress.pack_forget()
 
         self.recipe_tree = TreeviewWithSort(self, columns=("nq", "hq", "levels", "ingredients"),
                                             show="headings")
@@ -35,14 +40,39 @@ class SearchPage(RecipeListPage):
         treeview.heading("ingredients", text="Ingredients")
         treeview.column("levels", anchor=tk.CENTER)
 
-    def search_recipes(self, search_term):
+    def start_search_recipes(self):
+        self.search_button.config(state=tk.DISABLED)
         self.clear_treeview(self.recipe_tree)
-        results = RecipeController.search_recipe(search_term)
+        self.search_progress.pack_forget()
+        self.search_progress.pack(pady=10, before=self.recipe_tree)
+        self.search_progress.start()
+        threading.Thread(target=self.query_recipes, args=(self.search_var.get(),)).start()
 
-        for recipe in results:
+    def query_recipes(self, search_term):
+        results = RecipeController.search_recipe(search_term)
+        self.total_results = len(results)
+        self.results = results
+        self.after(10, self.update_search_progress, 0, [])
+
+    def update_search_progress(self, index, batch):
+        if index < self.total_results:
+            recipe = self.results[index]
             nq_string = recipe.get_formatted_nq_result()
             hq_string = recipe.get_formatted_hq_results()
             levels_string = recipe.get_formatted_levels_string()
             ingredient_names_summarized = recipe.get_formatted_ingredient_names()
             row = [nq_string, hq_string, levels_string, ingredient_names_summarized]
-            self.recipe_tree.insert("", "end", iid=recipe.id, values=row)
+            batch.append((recipe.id, row))
+
+            if len(batch) >= 10:
+                for item in batch:
+                    self.recipe_tree.insert("", "end", iid=item[0], values=item[1])
+                batch.clear()
+
+            self.after(10, self.update_search_progress, index + 1, batch)
+        else:
+            for item in batch:
+                self.recipe_tree.insert("", "end", iid=item[0], values=item[1])
+            self.search_progress.stop()
+            self.search_progress.pack_forget()
+            self.search_button.config(state=tk.NORMAL)
