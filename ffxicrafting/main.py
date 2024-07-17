3b786c31f8b1a5162c7aac24a37f11134781c64b
marcwalkr <47399441+marcwalkr@users.noreply.github.com>
2024-07-17 02:16:35 -0500

    Add setup and requirements

--- ffxicrafting/main.py
+++ ffxicrafting/main.py
@@ -41,6 +41,10 @@ class App(tk.Tk):
         SettingsPage(self)
 
 
-if __name__ == "__main__":
+def main():
     app = App()
     app.mainloop()
+
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ffxicrafting",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "tkinter",
+        "mysql-connector-python",
+        "python-dotenv"
+    ],
+    entry_points={
+        "console_scripts": [
+            "ffxicrafting=ffxicrafting.main:main",
+        ],
+    },
+)
