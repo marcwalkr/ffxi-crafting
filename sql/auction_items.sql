44bcfc8969d0acdf80ddf8f39428d339cfc21ae6
marcwalkr <47399441+marcwalkr@users.noreply.github.com>
2024-07-20 22:52:29 -0500

    Remove old auction_items.sql

--- sql/auction_items.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE IF EXISTS auction_items;
-CREATE TABLE auction_items (
-    itemid smallint UNSIGNED PRIMARY KEY,
-    single_price int UNSIGNED NOT NULL,
-    stack_price int UNSIGNED NOT NULL
-);
