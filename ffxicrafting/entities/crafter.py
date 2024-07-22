0c5e378f06c05d45cfbab35f725917299f659c7d
marcwalkr <47399441+marcwalkr@users.noreply.github.com>
2024-07-22 11:32:57 -0500

    Refactor Crafter craft() method

--- ffxicrafting/entities/crafter.py
+++ ffxicrafting/entities/crafter.py
@@ -1,5 +1,5 @@
-from config import SettingsManager
 from entities import Synth
+from config import SettingsManager
 
 
 class Crafter:
@@ -14,55 +14,57 @@ class Crafter:
         self.cook = cook
         self.recipe = recipe
         self.synth = Synth(recipe, self)
-        self.num_times = SettingsManager.get_simulation_trials()
 
-    def craft(self):
-        results, retained_ingredients = self.synth.simulate(self.num_times)
+    def craft(self, item_controller):
+        num_trials = SettingsManager.get_simulation_trials()
+        simulation_cost, results = self.synth.simulate(num_trials, item_controller)
 
-        # Calculate the total cost saved from retained ingredients after failures
-        simulation_cost = self.synth.cost * self.num_times
-        simulation_cost -= self._get_saved_cost(retained_ingredients)
+        if not simulation_cost or not results:
+            return [], None, None
 
-        # Calculate the expected profit for each result, selling them as singles and stacks
-        # Dictionaries in the form result: gil
-        single_profits = self._get_single_profits(results, simulation_cost)
-        stack_profits = self._get_stack_profits(results, simulation_cost)
+        # Calculate and set the expected profit for each result, selling them as singles and stacks
+        self._set_results_profit(results, simulation_cost, item_controller)
 
         # Calculate the overall expected profit per synth and per character storage unit
-        profit_per_synth = self._get_profit_per_synth(results, simulation_cost, self.num_times)
+        profit_per_synth = self._get_profit_per_synth(results, simulation_cost, num_trials)
         profit_per_storage = self._get_profit_per_storage(results, simulation_cost)
 
-        return single_profits, stack_profits, int(profit_per_synth), int(profit_per_storage)
+        return results.keys(), int(profit_per_synth), int(profit_per_storage)
 
-    def _get_saved_cost(self, retained_ingredients):
-        total_saved_cost = 0
-        for ingredient, amount in retained_ingredients.items():
-            saved_cost = ingredient.min_price * amount
-            total_saved_cost += saved_cost
-
-        return total_saved_cost
-
-    def _calculate_profits(self, results, simulation_cost, price_attr, stack_size_attr, single):
-        profits = {}
+    def _set_results_profit(self, results, simulation_cost, item_controller):
         for result, quantity in results.items():
-            price = getattr(result, price_attr)
-            stack_size = getattr(result, stack_size_attr)
-            if price is None or (not single and stack_size == 1):
-                profits[result] = None
-                continue
-            cost_per_item = simulation_cost / quantity
-            if single:
-                profit = price - cost_per_item
-            else:
-                profit = price - cost_per_item * stack_size
-            profits[result] = int(profit)
-        return profits
+            item_controller.update_auction_data(result.item_id)
+            result.single_profit = self._get_single_profit(result, quantity, simulation_cost)
+            result.stack_profit = self._get_stack_profit(result, quantity, simulation_cost)
 
-    def _get_single_profits(self, results, simulation_cost):
-        return self._calculate_profits(results, simulation_cost, "single_price", "stack_size", single=True)
+    def _get_single_profit(self, result, quantity, simulation_cost):
+        return self._calculate_profit(result, quantity, simulation_cost, "single_price", "stack_size", single=True)
 
-    def _get_stack_profits(self, results, simulation_cost):
-        return self._calculate_profits(results, simulation_cost, "stack_price", "stack_size", single=False)
+    def _get_stack_profit(self, result, quantity, simulation_cost):
+        return self._calculate_profit(result, quantity, simulation_cost, "stack_price", "stack_size", single=False)
+
+    def _calculate_profit(self, result, quantity, simulation_cost, price_attr, stack_size_attr, single=True):
+        price = getattr(result, price_attr)
+        stack_size = getattr(result, stack_size_attr)
+        if price is None or (not single and stack_size == 1):
+            return None
+        cost_per_item = simulation_cost / quantity
+        if single:
+            profit = price - cost_per_item
+        else:
+            profit = price - cost_per_item * stack_size
+        return int(profit)
+
+    def _get_profit_per_synth(self, results, simulation_cost, num_trials):
+        total_profit = self._calculate_total_profit(results, simulation_cost)
+        profit_per_synth = total_profit / num_trials
+        return profit_per_synth
+
+    def _get_profit_per_storage(self, results, simulation_cost):
+        total_profit = self._calculate_total_profit(results, simulation_cost)
+        total_storage = self._calculate_total_storage(results)
+        profit_per_storage = total_profit / total_storage if total_storage > 0 else 0
+        return profit_per_storage
 
     def _calculate_total_profit(self, results, simulation_cost):
         total_gil = 0
@@ -100,14 +102,3 @@ class Crafter:
                 total_storage += quantity / result.stack_size
 
         return total_storage
-
-    def _get_profit_per_synth(self, results, simulation_cost, num_trials):
-        total_profit = self._calculate_total_profit(results, simulation_cost)
-        profit_per_synth = total_profit / num_trials
-        return profit_per_synth
-
-    def _get_profit_per_storage(self, results, simulation_cost):
-        total_profit = self._calculate_total_profit(results, simulation_cost)
-        total_storage = self._calculate_total_storage(results)
-        profit_per_storage = total_profit / total_storage if total_storage > 0 else 0
-        return profit_per_storage
--- ffxicrafting/entities/synth.py
+++ ffxicrafting/entities/synth.py
@@ -1,6 +1,7 @@
 import random
 from collections import defaultdict
 from utils import clamp
+from config import SettingsManager
 
 
 class Synth:
@@ -42,7 +43,7 @@ class Synth:
         skill_differences = [recipe - crafter for recipe, crafter in zip(recipe_skills, crafter_skills)]
         return max(skill_differences, default=0)
 
-    def get_tier(self):
+    def _get_tier(self):
         if self.difficulty < -50:
             return 3
         elif self.difficulty < -30:
@@ -54,7 +55,7 @@ class Synth:
         else:
             return -1
 
-    def attempt_success(self):
+    def _attempt_success(self):
         success_probability = self.SUCCESS_PROBABILITY - \
             (self.difficulty / 10) if self.tier == -1 else self.SUCCESS_PROBABILITY
         if self.recipe.desynth:
@@ -62,26 +63,26 @@ class Synth:
                 (self.difficulty / 10) if self.tier == -1 else self.DESYNTH_SUCCESS_PROBABILITY
         return random.random() < max(success_probability, self.MIN_SUCCESS_PROBABILITY)
 
-    def attempt_hq(self):
+    def _attempt_hq(self):
         if self.recipe.desynth:
             hq_probability = self.DESYNTH_HQ_PROBABILITIES[self.tier + 1]
         else:
             hq_probability = self.HQ_PROBABILITIES[self.tier + 1]
         return random.random() < hq_probability
 
-    def get_hq_tier(self):
+    def _get_hq_tier(self):
         weights = self.DESYNTH_HQ_TIER_WEIGHTS if self.recipe.desynth else self.HQ_TIER_WEIGHTS
         return random.choices([1, 2, 3], weights=weights)[0]
 
-    def synth(self):
-        if self.attempt_success():
-            if self.attempt_hq():
-                hq_tier = self.get_hq_tier()
-                return self.get_hq_result(hq_tier)
+    def _synth(self):
+        if self._attempt_success():
+            if self._attempt_hq():
+                hq_tier = self._get_hq_tier()
+                return self._get_hq_result(hq_tier)
             return self.recipe.result, self.recipe.result_qty
         return None, None
 
-    def get_hq_result(self, hq_tier):
+    def _get_hq_result(self, hq_tier):
         if hq_tier == 1:
             return self.recipe.result_hq1, self.recipe.result_hq1_qty
         elif hq_tier == 2:
@@ -89,7 +90,7 @@ class Synth:
         else:
             return self.recipe.result_hq3, self.recipe.result_hq3_qty
 
-    def do_synth_fail(self):
+    def _do_synth_fail(self):
         loss_probability = clamp(0.15 - (self.difficulty / 20), 0, 1) if self.difficulty > 0 else 0.15
         if self.recipe.desynth:
             loss_probability += 0.35
@@ -103,26 +104,48 @@ class Synth:
 
         return retained_ingredients
 
-    def calculate_cost(self):
+    def _calculate_cost(self, item_controller):
+        self._set_ingredients_cost(item_controller)
         total_cost = 0
         for ingredient in self.recipe.get_ingredients():
             if ingredient.min_price is None:
                 return None
             total_cost += ingredient.min_price
-
         return int(total_cost)
 
-    def simulate(self, num_times):
+    def _set_ingredients_cost(self, item_controller):
+        for ingredient in self.recipe.get_ingredients():
+            item_controller.update_vendor_data(ingredient.item_id)
+            item_controller.update_auction_data(ingredient.item_id)
+
+    def simulate(self, num_trials, item_controller):
+        self.cost = self._calculate_cost(item_controller)
+        if not self.cost:
+            return None, None
+
         results = defaultdict(lambda: 0)
         retained_ingredients = defaultdict(lambda: 0)
 
-        for _ in range(num_times):
-            result, quantity = self.synth()
+        for _ in range(num_trials):
+            result, quantity = self._synth()
             if result is not None:
                 results[result] += quantity
             else:
-                retained = self.do_synth_fail()
+                retained = self._do_synth_fail()
                 for result_retained, quantity_retained in retained.items():
                     retained_ingredients[result_retained] += quantity_retained
 
-        return results, retained_ingredients
+        simulation_cost = self.cost * num_trials
+
+        # Subtract the total cost saved from retained ingredients after failures
+        simulation_cost -= self._get_saved_cost(retained_ingredients)
+
+        return simulation_cost, results
+
+    def _get_saved_cost(self, retained_ingredients):
+        total_saved_cost = 0
+        for ingredient, amount in retained_ingredients.items():
+            saved_cost = ingredient.min_price * amount
+            total_saved_cost += saved_cost
+
+        return total_saved_cost
