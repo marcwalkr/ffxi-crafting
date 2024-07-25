1d0f0bfb303078a3de99470ab968b3b4ba9009fb
marcwalkr <47399441+marcwalkr@users.noreply.github.com>
2024-07-25 11:07:59 -0500

    Remove last_updated from item costs table

--- sql/calculated_item_costs.sql
+++ sql/calculated_item_costs.sql
@@ -7,6 +7,5 @@ CREATE TABLE calculated_item_costs (
     beastmen_controlled_regions JSON,
     enabled_guilds JSON,
     auction_data_hash VARCHAR(32),
-    last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (item_id, recipe_id, craft_skills(255), beastmen_controlled_regions(255), enabled_guilds(255), auction_data_hash)
 );
