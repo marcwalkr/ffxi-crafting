e3bffec0c8311842a6920aa459ff3e756f606519
marcwalkr <47399441+marcwalkr@users.noreply.github.com>
2024-07-27 17:43:33 -0500

    Delete vendor files

--- vendor_files/Creepstix.lua
+++
@@ -1,6 +0,0 @@
-xi.eraShops.Creepstix =
-{
-    5023,   8160, -- Scroll of Goblin Gavotte
-    4734,   7074, -- Scroll of Protectra II
-    4738,   1700, -- Scroll of Shellra
-}
--- vendor_files/Nogga.lua
+++
@@ -1,5 +0,0 @@
-xi.eraShops.Nogga =
-{
-    17316,  675, 2, -- Bomb Arm
-    17313, 1083, 3, -- Grenade
-}
