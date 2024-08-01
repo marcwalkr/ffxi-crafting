9959429fb9abe5d387b0a5469b97a2562676a57c
marcwalkr <47399441+marcwalkr@users.noreply.github.com>
2024-08-01 18:24:24 -0500

    Move model object fetching to RecipeRepository

--- ffxicrafting/controllers/recipe_controller.py
+++ ffxicrafting/controllers/recipe_controller.py
@@ -7,16 +7,11 @@ logger = logging.getLogger(__name__)
 
 
 class RecipeController:
-    query_cache = {
-        "get_recipes_by_level": {},
-        "search_recipe": {}
-    }
     recipe_cache = {}
 
     def __init__(self, db) -> None:
-        self.db = db
-        self.item_controller = ItemController(self.db)
-        self.recipe_repository = RecipeRepository(self.db)
+        self.item_controller = ItemController(db)
+        self.recipe_repository = RecipeRepository(db)
 
     def get_recipe(self, recipe_id):
         if recipe_id in self.recipe_cache:
@@ -25,86 +20,77 @@ class RecipeController:
             raise ValueError(f"Recipe with id {recipe_id} not found in cache.")
 
     def get_recipes_by_level(self, *craft_levels, batch_size, offset):
-        cache_key = (craft_levels, batch_size, offset)
-        if cache_key in self.query_cache["get_recipes_by_level"]:
-            return self.query_cache["get_recipes_by_level"][cache_key]
-
-        recipe_tuples = self.db.get_recipes_by_level(*craft_levels, batch_size, offset)
-        if recipe_tuples:
-            recipes = self.process_and_cache_recipes(recipe_tuples)
-            self.query_cache["get_recipes_by_level"][cache_key] = recipes
+        recipe_models = self.recipe_repository.get_recipes_by_level(*craft_levels, batch_size=batch_size, offset=offset)
+        if recipe_models:
+            recipes = self.process_and_cache_recipes(recipe_models)
+            self.recipe_cache.update({recipe.id: recipe for recipe in recipes})
             return recipes
         else:
             return []
 
     def search_recipe(self, search_term, batch_size, offset):
-        cache_key = (search_term, batch_size, offset)
-        if cache_key in self.query_cache["search_recipe"]:
-            return self.query_cache["search_recipe"][cache_key]
-
-        recipe_tuples = self.db.search_recipe(search_term, batch_size, offset)
-        if recipe_tuples:
-            recipes = self.process_and_cache_recipes(recipe_tuples)
-            self.query_cache["search_recipe"][cache_key] = recipes
+        recipe_models = self.recipe_repository.search_recipe(search_term, batch_size=batch_size, offset=offset)
+        if recipe_models:
+            recipes = self.process_and_cache_recipes(recipe_models)
+            self.recipe_cache.update({recipe.id: recipe for recipe in recipes})
             return recipes
         else:
             return []
 
-    def process_and_cache_recipes(self, recipe_tuples):
+    def process_and_cache_recipes(self, recipe_models):
         ingredient_item_ids = set()
         result_item_ids = set()
 
-        for recipe_tuple in recipe_tuples:
+        for recipe_model in recipe_models:
             # Treat the crystal as an ingredient
-            ingredient_item_ids.add(recipe_tuple[11])
+            ingredient_item_ids.add(recipe_model.crystal)
 
-            # Gather ingredient ids, excluding empty ingredients represented by 0
-            ingredient_item_ids.update(id for id in recipe_tuple[13:21] if id != 0)
+            # Gather ingredient ids
+            ingredient_item_ids.update([recipe_model.ingredient1, recipe_model.ingredient2, recipe_model.ingredient3,
+                                       recipe_model.ingredient4, recipe_model.ingredient5, recipe_model.ingredient6,
+                                       recipe_model.ingredient7, recipe_model.ingredient8])
+
+            # Remove empty ingredients represented by 0
+            ingredient_item_ids.discard(0)
 
             # Gather result ids
-            result_item_ids.update(recipe_tuple[21:25])
+            result_item_ids.update([recipe_model.result, recipe_model.result_hq1, recipe_model.result_hq2,
+                                   recipe_model.result_hq3])
 
         ingredients, results = self.item_controller.get_recipe_items(list(ingredient_item_ids), list(result_item_ids))
 
         recipes = []
-        for recipe_tuple in recipe_tuples:
-            recipe_id = recipe_tuple[0]
-            if recipe_id in self.recipe_cache:
-                recipes.append(self.recipe_cache[recipe_id])
+        for recipe_model in recipe_models:
+            if recipe_model.id in self.recipe_cache:
+                recipes.append(self.recipe_cache[recipe_model.id])
             else:
-                recipe = self.create_recipe_object(recipe_tuple, ingredients, results)
-                self.recipe_cache[recipe_id] = recipe
+                recipe = self.create_recipe_object(recipe_model, ingredients, results)
+                self.recipe_cache[recipe_model.id] = recipe
                 recipes.append(recipe)
         return recipes
 
-    def create_recipe_object(self, recipe_tuple, ingredients, results):
-        crystal_id = recipe_tuple[11]
-        hq_crystal_id = recipe_tuple[12]
-        ingredient_ids = recipe_tuple[13:21]
-        result_ids = recipe_tuple[21:25]
-        result_quantities = recipe_tuple[25:29]
-        result_name = recipe_tuple[29]
-
+    def create_recipe_object(self, recipe_model, ingredients, results):
         recipe = Recipe(
-            *recipe_tuple[:11],
-            crystal_id,
-            hq_crystal_id,
-            *ingredient_ids,
-            *result_ids,
-            *result_quantities,
-            result_name,
-            next((ingredient for ingredient in ingredients if ingredient.item_id == crystal_id), None),
-            next((ingredient for ingredient in ingredients if ingredient.item_id == ingredient_ids[0]), None),
-            next((ingredient for ingredient in ingredients if ingredient.item_id == ingredient_ids[1]), None),
-            next((ingredient for ingredient in ingredients if ingredient.item_id == ingredient_ids[2]), None),
-            next((ingredient for ingredient in ingredients if ingredient.item_id == ingredient_ids[3]), None),
-            next((ingredient for ingredient in ingredients if ingredient.item_id == ingredient_ids[4]), None),
-            next((ingredient for ingredient in ingredients if ingredient.item_id == ingredient_ids[5]), None),
-            next((ingredient for ingredient in ingredients if ingredient.item_id == ingredient_ids[6]), None),
-            next((ingredient for ingredient in ingredients if ingredient.item_id == ingredient_ids[7]), None),
-            next((result for result in results if result.item_id == result_ids[0]), None),
-            next((result for result in results if result.item_id == result_ids[1]), None),
-            next((result for result in results if result.item_id == result_ids[2]), None),
-            next((result for result in results if result.item_id == result_ids[3]), None)
+            recipe_model.id, recipe_model.desynth, recipe_model.key_item, recipe_model.wood, recipe_model.smith,
+            recipe_model.gold, recipe_model.cloth, recipe_model.leather, recipe_model.bone, recipe_model.alchemy,
+            recipe_model.cook, recipe_model.crystal, recipe_model.hq_crystal, recipe_model.ingredient1,
+            recipe_model.ingredient2, recipe_model.ingredient3, recipe_model.ingredient4, recipe_model.ingredient5,
+            recipe_model.ingredient6, recipe_model.ingredient7, recipe_model.ingredient8, recipe_model.result,
+            recipe_model.result_hq1, recipe_model.result_hq2, recipe_model.result_hq3, recipe_model.result_qty,
+            recipe_model.result_hq1_qty, recipe_model.result_hq2_qty, recipe_model.result_hq3_qty,
+            recipe_model.result_name,
+            next((ingredient for ingredient in ingredients if ingredient.item_id == recipe_model.crystal), None),
+            next((ingredient for ingredient in ingredients if ingredient.item_id == recipe_model.ingredient1), None),
+            next((ingredient for ingredient in ingredients if ingredient.item_id == recipe_model.ingredient2), None),
+            next((ingredient for ingredient in ingredients if ingredient.item_id == recipe_model.ingredient3), None),
+            next((ingredient for ingredient in ingredients if ingredient.item_id == recipe_model.ingredient4), None),
+            next((ingredient for ingredient in ingredients if ingredient.item_id == recipe_model.ingredient5), None),
+            next((ingredient for ingredient in ingredients if ingredient.item_id == recipe_model.ingredient6), None),
+            next((ingredient for ingredient in ingredients if ingredient.item_id == recipe_model.ingredient7), None),
+            next((ingredient for ingredient in ingredients if ingredient.item_id == recipe_model.ingredient8), None),
+            next((result for result in results if result.item_id == recipe_model.result), None),
+            next((result for result in results if result.item_id == recipe_model.result_hq1), None),
+            next((result for result in results if result.item_id == recipe_model.result_hq2), None),
+            next((result for result in results if result.item_id == recipe_model.result_hq3), None)
         )
         return recipe
--- ffxicrafting/repositories/recipe_repository.py
+++ ffxicrafting/repositories/recipe_repository.py
@@ -2,10 +2,38 @@ from models import RecipeModel
 
 
 class RecipeRepository:
+    query_cache = {
+        "get_recipes_by_level": {},
+        "search_recipe": {}
+    }
     all_result_item_ids = None
 
     def __init__(self, db):
         self.db = db
+
+    def get_recipes_by_level(self, *craft_levels, batch_size, offset):
+        cache_key = (craft_levels, batch_size, offset)
+        if cache_key in self.query_cache["get_recipes_by_level"]:
+            return self.query_cache["get_recipes_by_level"][cache_key]
+
+        recipe_tuples = self.db.get_recipes_by_level(*craft_levels, batch_size, offset)
+        return self.cache_and_return_recipes(recipe_tuples, cache_key, "get_recipes_by_level")
+
+    def search_recipe(self, search_term, batch_size, offset):
+        cache_key = (search_term, batch_size, offset)
+        if cache_key in self.query_cache["search_recipe"]:
+            return self.query_cache["search_recipe"][cache_key]
+
+        recipe_tuples = self.db.search_recipe(search_term, batch_size, offset)
+        return self.cache_and_return_recipes(recipe_tuples, cache_key, "search_recipe")
+
+    def cache_and_return_recipes(self, recipe_tuples, cache_key, cache_name):
+        if recipe_tuples:
+            recipe_models = [RecipeModel(*recipe_tuple) for recipe_tuple in recipe_tuples]
+            self.query_cache[cache_name][cache_key] = recipe_models
+            return recipe_models
+        else:
+            return []
 
     def get_all_result_item_ids(self):
         if self.all_result_item_ids is None:
