c109c5fc2989588385599d5d1da1744e2d62236a
marcwalkr <47399441+marcwalkr@users.noreply.github.com>
2024-08-02 09:50:37 -0500

    Add type hints and underscores to main

--- ffxicrafting/main.py
+++ ffxicrafting/main.py
@@ -14,66 +14,69 @@ class App(tk.Tk):
     of the application using a notebook interface.
     """
 
-    def __init__(self):
+    def __init__(self) -> None:
         """Initialize the main application window and set up its components."""
         super().__init__()
         self.title("FFXI Crafting Tool")
         self.geometry("1600x900")
 
-        self.recipe_pages = []
+        self._recipe_pages: list[RecipeListPage] = []
 
-        self.configure_styles()
-        self.create_main_frame()
-        self.create_notebook()
-        self.create_pages()
+        self._main_frame: ttk.Frame = None
+        self.notebook: ttk.Notebook = None
+
+        self._configure_styles()
+        self._create_main_frame()
+        self._create_notebook()
+        self._create_pages()
 
         self.protocol("WM_DELETE_WINDOW", self.on_close)
 
-    def configure_styles(self):
+    def _configure_styles(self) -> None:
         """Configure the styles for various UI elements."""
-        self.style = ttk.Style(self)
-        self.style.configure("TButton", font=("Helvetica", 14), padding=10)
-        self.style.configure("TLabel", font=("Helvetica", 14), padding=5)
-        self.style.configure("TNotebook.Tab", font=("Helvetica", 10), padding=4)
-        self.style.configure("Treeview", font=("Helvetica", 12))
-        self.style.configure("Treeview.Heading", font=("Helvetica", 12, "bold"))
-        self.style.configure("Custom.TCheckbutton", font=("Helvetica", 14))
-        self.style.configure("TMenubutton", font=("Helvetica", 12))
+        style = ttk.Style(self)
+        style.configure("TButton", font=("Helvetica", 14), padding=10)
+        style.configure("TLabel", font=("Helvetica", 14), padding=5)
+        style.configure("TNotebook.Tab", font=("Helvetica", 10), padding=4)
+        style.configure("Treeview", font=("Helvetica", 12))
+        style.configure("Treeview.Heading", font=("Helvetica", 12, "bold"))
+        style.configure("Custom.TCheckbutton", font=("Helvetica", 14))
+        style.configure("TMenubutton", font=("Helvetica", 12))
 
-    def create_main_frame(self):
+    def _create_main_frame(self) -> None:
         """Create and set up the main frame of the application."""
-        self.main_frame = ttk.Frame(self)
-        self.main_frame.pack(expand=True, fill="both")
+        self._main_frame = ttk.Frame(self)
+        self._main_frame.pack(expand=True, fill="both")
 
-    def create_notebook(self):
+    def _create_notebook(self) -> None:
         """Create the notebook widget to hold different pages."""
-        self.notebook = ttk.Notebook(self.main_frame)
+        self.notebook = ttk.Notebook(self._main_frame)
         self.notebook.pack(expand=True, fill="both")
 
-    def create_pages(self):
+    def _create_pages(self) -> None:
         """Create and initialize the different pages of the application."""
-        self.pages = [
+        pages = [
             SearchPage(self),
             ProfitPage(self),
             SettingsPage(self)
         ]
-        for page in self.pages:
+        for page in pages:
             if isinstance(page, RecipeListPage):
-                self.recipe_pages.append(page)
+                self._recipe_pages.append(page)
 
-    def on_close(self):
+    def on_close(self) -> None:
         """Handle the application closing event."""
-        self.cleanup_pages()
+        self._cleanup_pages()
         self.destroy()
 
-    def cleanup_pages(self):
+    def _cleanup_pages(self) -> None:
         """Clean up resources used by recipe pages before closing."""
-        for page in self.recipe_pages:
+        for page in self._recipe_pages:
             page.cleanup()
-        self.recipe_pages.clear()
+        self._recipe_pages.clear()
 
 
-def main():
+def main() -> None:
     """
     Main function to run the FFXI Crafting Tool application.
 
