7450d7081bb95c9777c465c8327913bbf7aa813c
marcwalkr <47399441+marcwalkr@users.noreply.github.com>
2024-08-04 01:21:26 -0500

    Add ItemModel docstrings

--- ffxicrafting/models/item_model.py
+++ ffxicrafting/models/item_model.py
@@ -1,12 +1,34 @@
 class ItemModel:
-    def __init__(self, item_id, sub_id, name, sort_name, stack_size, flags, ah,
-                 no_sale, base_sell) -> None:
-        self.item_id = item_id
-        self.sub_id = sub_id
-        self.name = name
-        self.sort_name = sort_name
-        self.stack_size = stack_size
-        self.flags = flags
-        self.ah = ah
-        self.no_sale = no_sale
-        self.base_sell = base_sell
+    """
+    Represents an item in the game.
+
+    This class corresponds to a database table that stores basic information about
+    game items. It includes data such as item identifiers, names, stack sizes,
+    and various flags and categories.
+    """
+
+    def __init__(self, item_id: int, sub_id: int, name: str, sort_name: str, stack_size: int, flags: int, ah: int,
+                 no_sale: bool, base_sell: int) -> None:
+        """
+        Initialize an Item instance.
+
+        Args:
+            item_id (int): The unique identifier for the item.
+            sub_id (int): A secondary identifier for the item. Exact purpose unknown.
+            name (str): The name of the item as it appears in the game.
+            sort_name (str): A version of the name used for sorting purposes.
+            stack_size (int): The maximum quantity of this item that can be stacked in one inventory slot.
+            flags (int): A bitfield representing various item properties. Exact meanings unknown.
+            ah (int): The auction house category for this item.
+            no_sale (bool): True if the item cannot be sold to NPCs, False otherwise.
+            base_sell (int): The base selling price of the item to NPCs.
+        """
+        self.item_id: int = item_id
+        self.sub_id: int = sub_id
+        self.name: str = name
+        self.sort_name: str = sort_name
+        self.stack_size: int = stack_size
+        self.flags: int = flags
+        self.ah: int = ah
+        self.no_sale: bool = no_sale
+        self.base_sell: int = base_sell
