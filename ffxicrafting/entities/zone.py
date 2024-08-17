20e99e799fa6e15e2b1fd481ed3fa69c29f0499c
marcwalkr <47399441+marcwalkr@users.noreply.github.com>
2024-08-17 15:14:38 -0500

    Fix docstrings for Zone

--- ffxicrafting/entities/zone.py
+++ ffxicrafting/entities/zone.py
@@ -10,6 +10,11 @@ class Zone(ZoneModel):
     def __init__(self, zone_id: int, name: str) -> None:
         """
         Initialize a Zone instance.
+        Formats the zone name by replacing underscores with spaces.
+
+        Args:
+            zone_id (int): The ID of the zone.
+            name (str): The name of the zone.
         """
         super().__init__(zone_id, name)
         self.name = name.replace("_", " ")
